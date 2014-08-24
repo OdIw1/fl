5580702060273f513448872ea1f7226c7615da02
Sergey Koval <twilightinnocence@gmail.com>
2014-08-24 18:45:40 +0300

    initial pulse generation

--- NLSE.jl
+++ NLSE.jl
@@ -6,13 +6,24 @@ function t_grid(n, T)
 # where T0 is pulse FWHM;  in Julia time window is (-T, T)
 # also 'n' is no longer binary log of the number of grid points, it is the number itself
     dt = 2 * T / n
-    dt * (-n/2 : n/2 - 1)
+    dt * [-n/2 : (n/2 - 1)]
 end
 
 function w_grid(n, T)
     dw = pi / T
-    dw * [(0 : n/2 -1), (-n/2 : -1)]
+    dw * [0 : (n/2 -1), -n/2 : -1]
 end
 
+function secant_pulse(T0, P0, C0, t, t_offset=0)
+    t_scaled = (t - t_offset) / T0
+    sqrt(P0) * sech(t_scaled) .* exp(-0.5im * C0 * (t_scaled).^2)
+end
 
+function gaussian_pulse(m_order, T0, P0, C0, t, t_offset)
+    t_scaled = (t - t_offset) / T0
+    sqrt(P0) * exp(-0.5 * (1 + 1im* C0) * t_scaled .^ (2 * m_order))
+end
 
+function integrate_RK4IP(alpha, beta, gamma, steep, t_raman, L, h0, u0)
+
+end
